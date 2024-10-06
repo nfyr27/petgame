b1c6a4a3b2310e5fc382934a462cdddde4555e13
Богдан <gubkin.BOGDA@yandex.ru>
2024-10-06 12:14:16 +1000

    A loss was added. Updating food and clothing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
-
+import os
 import pygame as pg
+import json
 
 # Инициализация pg
 pg.init()
@@ -21,7 +22,10 @@ PADDING = 8
 MENU_NAV_XPAD = 90
 MENU_NAV_YPAD = 130
 
+TOY_SIZE = 100
+
 font = pg.font.Font(None, 40)
+maxi_font = pg.font.Font(None, 100)
 mini_font = pg.font.Font(None, 15)
 
 
@@ -39,7 +43,7 @@ class Food:
     def __init__(self, name, price, file, satiety, medicine_power=0, happiness=0):
         self.name = name
         self.price = price
-        self.image = load_image(file, DOG_WIDTH // 1.7, DOG_HEIGHT // 1.7)
+        self.image = load_image(file, DOG_WIDTH // 1.2, DOG_WIDTH // 1.2)
         self.satiety = satiety
         self.medicine_power = medicine_power
         self.happiness = happiness
@@ -55,9 +59,11 @@ class FoodMenu:
         self.top_label_off = load_image("images/menu/top_label_off.png", SCREEN_WIDTH, SCREEN_HEIGHT)
         self.top_label_on = load_image("images/menu/top_label_on.png", SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.items = [Food("Яблоко", 15, "images/food/apple.png", 5),
-                      Food("Кость", 20, "images/food/bone.png", 10),
-                      Food("Мясо", 40, "images/food/meat.png", 30)]
+        self.items = [Food("Яблоко", 15, "images/food/apple.png", 5, 2, 2),
+                      Food("Кость", 20, "images/food/bone.png", 7, 0, 3),
+                      Food("Мясо", 40, "images/food/meat.png", 20, 3 , 5),                      Food("Собачий корм", 40, "images/food/dog food.png", 30, 0, 10),
+                      Food("Элитный корм", 50, "images/food/dog food elite.png", 40, 0, 15),
+                      Food("Лекарство", 30, "images/food/medicine.png", 0, 20, -10)]
 
         self.current_item = 0
 
@@ -114,6 +120,12 @@ class FoodMenu:
             self.game.satiety += self.items[self.current_item].satiety
             if self.game.satiety > 100:
                 self.game.satiety = 100
+            self.game.health += self.items[self.current_item].medicine_power
+            if self.game.health > 100:
+                self.game.health = 100
+            self.game.happiness += self.items[self.current_item].happiness
+            if self.game.happiness > 100:
+                self.game.happiness = 100
 
     def update(self):
         self.next_button.update()
@@ -144,13 +156,14 @@ class Item:
         self.price = price
         self.is_bought = is_bought
         self.is_put_on = is_put_on
+        self.file = file
 
         self.image = load_image(file, DOG_WIDTH // 1.7, DOG_HEIGHT // 1.7)
         self.full_image = load_image(file, DOG_WIDTH, DOG_HEIGHT)
 
 
 class ClothesMenu:
-    def __init__(self, game):
+    def __init__(self, game, data):
         self.game = game
         self.menu_page = load_image("images/menu/menu_page.png", SCREEN_WIDTH, SCREEN_HEIGHT)
 
@@ -159,10 +172,9 @@ class ClothesMenu:
         self.top_label_off = load_image("images/menu/top_label_off.png", SCREEN_WIDTH, SCREEN_HEIGHT)
         self.top_label_on = load_image("images/menu/top_label_on.png", SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.items = [Item("Синяя футболка", 10, "images/items/blue t-shirt.png"),
-                      Item("Красная футболка", 10, "images/items/red t-shirt.png"),
-                      Item("Очки", 15, "images/items/sunglasses.png"),
-                      Item("Ботинки", 50, "images/items/boots.png")]
+        self.items = []
+        for item in data:
+            self.items.append(Item(*item.values()))
 
         self.current_item = 0
 
@@ -309,6 +321,80 @@ class Button:
             self.is_pressed = False
 
 
+class Toy(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        file = random.choice(os.listdir("images/toys"))
+        self.image = load_image(f"images/toys/{file}", TOY_SIZE, TOY_SIZE)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(MENU_NAV_XPAD, SCREEN_WIDTH - MENU_NAV_XPAD - TOY_SIZE)
+        self.rect.y = MENU_NAV_YPAD - TOY_SIZE
+
+    def update(self):
+        self.rect.y += 1
+
+
+class Dog(pg.sprite.Sprite):
+    def __init__(self, file):
+        self.file = file
+        super().__init__(),
+        self.image = pg.image.load("images/dog.png")
+        self.image = pg.transform.scale(self.image, (DOG_WIDTH // 2, DOG_HEIGHT // 2))
+        self.rect = self.image.get_rect()
+        self.rect.center = SCREEN_WIDTH // 2, SCREEN_HEIGHT - MENU_NAV_YPAD
+
+    def update(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.rect.x -= 1
+        if keys[pg.K_d]:
+            self.rect.x += 1
+
+
+class MiniGame:
+    def __init__(self, game):
+        self.game = game
+        self.background = load_image("images/game_background.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        self.dog = Dog("images/dog.png")
+
+        self.toys = pg.sprite.Group()
+
+        self.score = 0
+
+        self.start_time = pg.time.get_ticks()
+        self.interval = 20000
+
+    def new_game(self):
+        self.dog = Dog("images/dog.png")
+        self.toys = pg.sprite.Group()
+
+        self.score = 0
+
+        self.start_time = pg.time.get_ticks()
+        self.interval = 20000
+
+    def draw(self, screen):
+        screen.blit(self.background, (0, 0))
+        screen.blit(self.dog.image, self.dog.rect)
+        self.toys.draw(screen)
+        screen.blit(text_render(self.score), (MENU_NAV_XPAD + 20, 80))
+
+    def is_clicked(self, event):
+        ...
+
+    def update(self):
+        self.dog.update()
+        self.toys.update()
+        if random.randint(0, 100) == 0:
+            self.toys.add(Toy())
+        hits = pg.sprite.spritecollide(self.dog, self.toys, True, pg.sprite.collide_rect_ratio(0.6))
+        self.score += len(hits)
+        if pg.time.get_ticks() - self.start_time > self.interval:
+            self.game.happiness += int(self.score // 2)
+            self.game.mode = "Main"
+
+
 class Game:
     def __init__(self):
 
@@ -318,13 +404,19 @@ class Game:
         pg.display.set_caption("Виртуальный питомец")
         self.background = load_image("images/background.png", SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.happiness = 100
-        self.satiety = 100
-        self.health = 100
+        with open("save.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
 
-        self.money = 10
-        self.coin_per_second = 1
-        self.costs_of_upgrade = {100: False, 1000: False, 5000: False, 10000: False}
+        self.happiness = data["happiness"]
+        self.satiety = data["satiety"]
+        self.health = data["health"]
+
+        self.money = data["money"]
+        self.coin_per_second = data["coin_per_second"]
+
+        self.costs_of_upgrade = {}
+        for key, value in data["cost_of_upgrade"].items():
+            self.costs_of_upgrade[int(key)] = value
 
         self.INCREASE_MONEY = pg.USEREVENT + 1
         self.DECREASE = pg.USEREVENT + 2
@@ -341,16 +433,16 @@ class Game:
 
         self.eat_button = Button("Еда", button_x, PADDING + ICON_SIZE, func=self.food_menu_on)
         self.clothes_button = Button("Одежда", button_x, PADDING + ICON_SIZE * 2, func=self.clothes_menu_on)
-        self.play_button = Button("Игры", button_x, PADDING + ICON_SIZE * 3)
+        self.play_button = Button("Игры", button_x, PADDING + ICON_SIZE * 3, func=self.mini_game_on)
         self.upgrade_button = Button("Улучшить", SCREEN_WIDTH - ICON_SIZE, 0,
                                      width=BUTTON_WIDTH // 3, height=BUTTON_HEIGHT // 3,
                                      text_font=mini_font, func=self.increase_money)
 
         self.buttons = [self.eat_button, self.clothes_button, self.play_button, self.upgrade_button]
 
-        self.clothes_menu = ClothesMenu(self)
-
+        self.clothes_menu = ClothesMenu(self, data["clothes"])
         self.food_menu = FoodMenu(self)
+        self.mini_game = MiniGame(self)
 
         self.run()
 
@@ -359,6 +451,10 @@ class Game:
 
     def food_menu_on(self):
         self.mode = "Food menu"
+
+    def mini_game_on(self):
+        self.mode = "Mini game"
+        self.mini_game.new_game()
 
     def run(self):
         while True:
@@ -376,18 +472,65 @@ class Game:
 
     def event(self):
         for event in pg.event.get():
-            if event.type == self. DECREASE:
-                chance = random.randint(1, 10)
-                if chance <= 5:
-                    self.satiety -= 1
-                elif 5 < chance < 9:
-                    self.happiness -= 1
-                else:
-                    self.health -= 1
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     self.mode = "Main"
             if event.type == pg.QUIT:
+                if self.mode == "Game over":
+                    data = {
+                        "happiness": 100,
+                        "satiety": 100,
+                        "health": 100,
+                        "money": 10,
+                        "coin_per_second": 1,
+                        "cost_of_upgrade": {
+                            "100": False,
+                            "500": False,
+                            "1000": False,
+                            "5000": False,
+                            "10000": False
+                        },
+                        "clothes": []
+                    }
+
+                    for item in self.clothes_menu.items:
+                        data["clothes"].append(
+                            {"name": item.name,
+                             "price": item.price,
+                             "file": item.file,
+                             "is_bought": False,
+                             "is_put_on": False
+                             }
+                        )
+                else:
+                    data = {
+                        "happiness": self.happiness,
+                        "satiety": self.satiety,
+                        "health": self.health,
+                        "money": self.money,
+                        "coin_per_second": self.coin_per_second,
+                        "cost_of_upgrade": {
+                            "100": self.costs_of_upgrade[100],
+                            "500": self.costs_of_upgrade[500],
+                            "1000": self.costs_of_upgrade[1000],
+                            "5000": self.costs_of_upgrade[5000],
+                            "10000": self.costs_of_upgrade[10000]
+
+                        },
+                        "clothes": []
+                    }
+
+                    for item in self.clothes_menu.items:
+                        data["clothes"].append(
+                            {"name": item.name,
+                             "price": item.price,
+                             "file": item.file,
+                             "is_bought": item.is_bought,
+                             "is_put_on": item.is_put_on
+                             }
+                        )
+                with open("save.json", "w", encoding="utf-8") as f:
+                    json.dump(data, f, ensure_ascii=False)
                 pg.quit()
                 exit()
             if self.mode == "Main":
@@ -397,16 +540,23 @@ class Game:
                 self.clothes_menu.is_clicked(event)
             elif self.mode == "Food menu":
                 self.food_menu.is_clicked(event)
+            elif self.mode == "Mini game":
+                self.mini_game.is_clicked(event)
 
-            if event.type == self.INCREASE_MONEY:
-                self.money += self.coin_per_second
+            if self.mode != "Game over":
+                if event.type == self.INCREASE_MONEY:
+                    self.money += self.coin_per_second
+                if event.type == self.DECREASE:
+                    chance = random.randint(1, 10)
+                    if chance <= 5:
+                        self.satiety -= 1
+                    elif 5 < chance < 9:
+                        self.happiness -= 1
+                    else:
+                        self.health -= 1
 
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-                self.money += self.coin_per_second
-
-            if self.mode != "Main":
-                self.clothes_menu.is_clicked(event)
-                self.food_menu.is_clicked(event)
+                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                    self.money += self.coin_per_second
 
     def update(self):
         if self.mode == "Main":
@@ -416,6 +566,11 @@ class Game:
             self.clothes_menu.update()
         elif self.mode == "Food menu":
             self.food_menu.update()
+        elif self.mode == "Mini game":
+            self.mini_game.update()
+
+        if self.satiety <= 0 or self.health <= 0 or self.happiness <= 0:
+            self.mode = "Game over"
 
     def draw(self):
         pg.display.flip()
@@ -441,6 +596,12 @@ class Game:
             self.clothes_menu.draw(self.screen)
         elif self.mode == "Food menu":
             self.food_menu.draw(self.screen)
+        elif self.mode == "Mini game":
+            self.mini_game.draw(self.screen)
+        elif self.mode == "Game over":
+            text = maxi_font.render("ИГРА ОКОНЧЕНА", True, "red")
+            text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+            self.screen.blit(text, text_rect)
 
 
 if __name__ == "__main__":
